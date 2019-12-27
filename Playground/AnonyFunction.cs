c3e6138811249baf40c8796e86765e1bbc108013
eric-lo <ericlo@cse.cuhk.edu.hk>
2019-12-27 14:31:19 +0800

    Initial check-in of module Playground

---
+++ Playground/AnonyFunction.cs
@@ -0,0 +1,48 @@
+﻿using System;
+delegate void NumberChanger(int n);
+namespace Playground
+{
+    class TestDelegate
+    {
+        static int num = 10;
+
+        public static void AddNum(int p)
+        {
+            num += p;
+            Console.WriteLine("Named Method: {0}", num);
+        }
+        public static void MultNum(int q)
+        {
+            num *= q;
+            Console.WriteLine("Named Method: {0}", num);
+        }
+        public static int getNum()
+        {
+            return num;
+        }
+        static void Main_test(string[] args)
+        {
+            //below is an anonymous method; like polymorphism 'function'
+            NumberChanger nc = delegate (int x) {
+                Console.WriteLine("Anonymous Method: {0}", x);
+            };
+
+            //calling the delegate using the anonymous method 
+            nc(10);
+
+            //instantiating the delegate using the named methods 
+            nc = new NumberChanger(AddNum);
+
+            //calling the delegate using the named methods 
+            nc(5);
+
+            //instantiating the delegate using another named methods 
+            nc = new NumberChanger(MultNum);
+
+            //calling the delegate using the named methods 
+            nc(2);
+            Console.ReadKey();
+        }
+    }
+
+}
---
+++ Playground/AsyncAwait.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace AsyncApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1");
+            var a = async1();
+            Console.WriteLine("2; won't come up immediately");
+            //var a = async2();
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Main");
+                System.Threading.Thread.Sleep(1000);
+            }
+            a.Wait(); //block/wait until the async call returns //missing it will go on without waitng async thread
+            Console.WriteLine("Main-end");
+        }
+
+        static async Task<int> async1()
+        {
+            Console.WriteLine("ASYN-start");
+            /* AWAIT means to yield back the control back to the caller
+             and I continue*/
+            var result = await Task.Run(() =>
+            {
+                for (int i=0; i< 10; i++)
+                {
+                    Console.WriteLine("ASYN");
+                    System.Threading.Thread.Sleep(1000);
+
+                }
+                return 888;
+            });
+
+            Console.WriteLine("ASYN-end");
+            return 777;
+        }
+
+        static async Task<int> async2()
+        {
+            /* async keyword only has 1 usage: allowing you to use await keyword inside */
+            /* this function is = sync function cause we don't use await keyword here */
+            Console.WriteLine("ASYN2-start");
+            
+                for (int i = 0; i < 2000; i++)
+                {
+                    System.Threading.Thread.Sleep(5);
+                }
+
+            Console.WriteLine("ASYN2-end");
+            return 777;
+        }
+    }
+}
